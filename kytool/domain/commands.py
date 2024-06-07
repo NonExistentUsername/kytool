07ed4b4adbf04112134ac1a1def96d71b94130de
Dmytro Maior <mayor4090Gmail.com>
2024-06-07 16:28:21 +0300

    ID for events and commands is not necessary and can be added by user easily

--- kytool/domain/commands.py
+++ kytool/domain/commands.py
@@ -1,6 +1,3 @@
-from uuid import uuid4
-
-
 class Command:
     """
     Represents a command that can be executed by the system.
@@ -13,14 +10,3 @@ class Command:
         """
         Initializes a new Command instance with a unique identifier.
         """
-        self.__id: str = str(uuid4())  # unique id of command
-
-    @property
-    def id(self) -> str:
-        """
-        Get id of command
-
-        Returns:
-            str: Command id
-        """
-        return self.__id
